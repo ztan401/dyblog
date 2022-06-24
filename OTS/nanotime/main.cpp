6cd6e0a5e6a9f3d5f398c759e5824cfada62d0a5
QuantABC <aidabloc@163.com>
2022-06-24 19:37:04 +0800

    del ots

--- OTS/nanotime/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "nanotimestamp.h"
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "now in nano: "<< kungfu::yijinjing::time::now_in_nano()<< std::endl;
-    std::cout << "now: "<< kungfu::yijinjing::time::strftime(kungfu::yijinjing::time::now_in_nano())<< std::endl;
-    return 0;
-}
--- OTS/pynanotime/pytest/test.py
+++
@@ -1,5 +0,0 @@
-from pynanotime import now_in_nano, num2time, time2num
-
-print(now_in_nano())
-print(num2time(now_in_nano()))
-print(time2num(num2time(now_in_nano())))
